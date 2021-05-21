f7d6d788a537272b6f746fa11089249457c10f05
Jalaluddin Qureshi <58306478+JalalQ@users.noreply.github.com>
2021-05-20 20:21:27 -0400

    Add files via upload

---
+++ lab7-1-countdown.js
@@ -0,0 +1,44 @@
+﻿/* LAB 7-1 - FINAL COUNTDOWN!! */
+//alert("Lab 7 - 1");//Please delete once you have confirmed your page is connected...seriously, if I see this popup when I check out your work, I will go coo-coo bananas.
+
+//create page load listener
+window.onload = pageReady;
+
+//create page load function
+function pageReady(){
+    //alert("All loaded now!");
+
+	//create variables for required HTML elements
+    var dateTodayElement = document.getElementById("todayData");
+    var dateDeadlineElement = document.getElementById("finalData");
+    var dateDiffElement = document.getElementById("dueData");
+	
+	//create variables for now date and due date
+    var now = new Date(); //fetches the current time and date.
+    var date1970 = now.getTime(); //number of milliseconds since Jan 1 1970 and now.
+    var dueDate = new Date(2020, 11, 16, 23, 59, 0, 0); //var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+    var dueDateMS = dueDate.getTime();
+    //console.log(dueDate);
+	
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+    var timeDifference = dueDateMS - date1970;
+    var msecInADay = 86400000;
+    var days = timeDifference/msecInADay;
+	
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+    var days = Math.floor(days);
+    
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+    dateTodayElement.innerHTML = now.toDateString();
+    dateDeadlineElement.innerHTML = dueDate.toDateString();
+	
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+    if (days<0) {
+        dateDiffElement.innerHTML = "Deadline has passed";
+    }
+    else {
+        dateDiffElement.innerHTML = days ;
+    }
+    
+    
+}
---
+++ lab7-2-clock.js
@@ -0,0 +1,61 @@
+﻿/* LAB 7.2 - STOP TIME */
+//alert("Lab 2 is connected");//delete once confirming your page is connected. 
+
+//create page load listener
+window.onload = pageReady;
+
+//create page load function
+function pageReady(){
+    
+	//create variables for required HTML elements
+    var hourElement = document.getElementById("hoursOut");
+    var minElement = document.getElementById("minsOut");
+    var secElement = document.getElementById("secsOut");
+    var btnStart = document.getElementById("btnStart");
+    var btnStop = document.getElementById("btnStop");
+    
+	//create time variable so all functions have access to it
+    
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+    function displayTime() {
+        var today = new Date();
+        
+        //gathers information about current hour, min and sec. 
+        //Add zero at the head if it is a single digit number.
+        var currentHour = addZero(today.getHours());
+        var currentMin = addZero(today.getMinutes());
+        var currentSec = addZero(today.getSeconds());
+        
+        // for formatting the display.
+        hourElement.innerHTML = currentHour + ":";
+        minElement.innerHTML = currentMin + ":";
+        secElement.innerHTML = currentSec;
+    }
+
+	//CREATE FUNCTION TO START THE CLOCK.
+	var timer; //we need to acces the timer variable outside the function scope.
+    
+    function startClock() {  
+        timer = setInterval(displayTime, 500);    
+    }
+    
+	//CREATE FUNCTION TO STOP THE CLOCK
+	function stopClock() {
+        clearInterval(timer);    
+    }
+    
+	// SET EVENT LISTENERS
+    btnStart.onclick = startClock;
+    btnStop.onclick = stopClock;
+    
+    
+    // BONUS ACTIVITY
+    function addZero(j) {
+        if (j<10) {
+            j = "0" + j;
+        }
+        return j;
+    }
+            
+    
+}
